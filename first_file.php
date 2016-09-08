9a2f860d6041010a326efdfa68ad96f10ac0f1db
paulvase <paulvase@greeny.cs.tlu.ee>
2016-09-08 13:11:06 +0300

    added my fist lines of code

---
+++ first_file.php
@@ -0,0 +1,14 @@
+<?php
+
+	$firstName = "Paul";
+	$lastName = "Vasemagi";
+
+	
+	//echo $firstName;
+	//echo $lastName;
+	
+	//KAhe stringi liitmiseks kasutan punkti
+	echo $firstName." ".$lastName;
+	
+	
+?>
